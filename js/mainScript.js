449622de42bb87a26a9f8604692a1585efbdae78
galiks <Galiks@mail.ru>
2018-05-26 18:32:01 +0400

    maybe ready

--- js/mainScript.js
+++ js/mainScript.js
@@ -13,19 +13,19 @@ function calcPhrase() {
     check(massife)
 
     var operations = {
-        "+" : function (x, y) {
+        "+": function (x, y) {
             return x + y
         },
 
-        "-" : function (x, y) {
+        "-": function (x, y) {
             return x - y
         },
 
-        "*" : function (x, y) {
+        "*": function (x, y) {
             return x * y
         },
 
-        "/" : function (x, y) {
+        "/": function (x, y) {
             return y !== 0 ? x / y : 0
         }
     };
@@ -82,7 +82,6 @@ function duplicateIsEvil() {
     }
     catch {
         document.getElementById("die").value = error
-        throw new Error()
     }
 }
 
@@ -90,51 +89,79 @@ function datetimeParse() {
 
     var date = new Date(document.getElementById("datetime_in").value)
     var pattern = document.getElementById("pattern").value
-    var result
+    var result = pattern.match(/[y]{2,4}|[m]{1,2}|[M]{1,4}|[h]{1,2}|[d]{1,2}|[s]{1,2}|[H]{1,2}/g)
 
-    pattern = pattern.replace(/yyyy/g, date.getFullYear())
-    pattern = pattern.replace(/yy/g, date.getFullYear() % 100 >= 10 ?
-        date.getFullYear() % 100 : "0" + date.getFullYear() % 100)
-    pattern = pattern.replace(/MMMM/g, date.toLocaleString("ru", { month: "long"} ))
-    pattern = pattern.replace(/MMM/g, date.toLocaleString("ru", { month: "short" }))
-    pattern = pattern.replace(/MM/g, date.getMonth() + 1 >= 10 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1))
-    pattern = pattern.replace(/M/g, date.getMonth() + 1)
-    pattern = pattern.replace(/dd/g, date.getDate() >= 10 ? date.getDate() : "0" + date.getDate())
-    pattern = pattern.replace(/d/g, date.getDate())
-    pattern = pattern.replace(/HH/g, date.getHours() >= 10 ? date.getHours() : "0" + date.getHours())
-    pattern = pattern.replace(/H/g, date.getHours())
-    pattern = pattern.replace(/hh/g, getHours(date.getHours(), true))
-    pattern = pattern.replace(/h/g, getHours(date.getHours(), false))
-    pattern = pattern.replace(/mm/g, date.getMinutes() >= 10 ? date.getMinutes() : "0" + date.getMinutes())
-    pattern = pattern.replace(/m/g, date.getMinutes())
-    pattern = pattern.replace(/ss/g, date.getSeconds() >= 10 ? date.getSeconds() : "0" + date.getSeconds())
-    pattern = pattern.replace(/s/g, date.getSeconds())
+    if (result.length > 0) {
+        for (let i = 0; i < result.length; i++) {
+            pattern = findAndReplace(result[i], pattern, date)
+        }
+    }
+    else {
+        document.getElementById("datetime_out").value = error
+    }
+
+
+    function findAndReplace(value, pattern, date) {
+        switch (value) {
+            case "yyyy":
+                return pattern.replace(/yyyy/g, date.getFullYear());
+            case "yy":
+                return pattern.replace(/yy/g, date.getFullYear() % 100 >= 10 ? date.getFullYear() % 100 : "0" + date.getFullYear() % 100);
+            case "MMMM":
+                return pattern.replace(/MMMM/g, date.toLocaleString("ru", {month: "long"}));
+            case "MMM":
+                return pattern.replace(/MMM/g, date.toLocaleString("ru", {month: "short"}));
+            case "MM":
+                return pattern.replace(/MM/g, date.getMonth() + 1 >= 10 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1));
+            case "M":
+                return pattern.replace(/M/g, date.getMonth() + 1);
+            case "dd":
+                return pattern.replace(/dd/g, date.getDate() >= 10 ? date.getDate() : "0" + date.getDate());
+            case "d":
+                return pattern.replace(/d/g, date.getDate());
+            case "HH":
+                return pattern.replace(/HH/g, date.getHours() >= 10 ? date.getHours() : "0" + date.getHours());
+            case "H":
+                return pattern.replace(/H/g, date.getHours());
+            case "hh":
+                return pattern.replace(/hh/g, getHour(date.getHours(), true));
+            case "h":
+                return pattern.replace(/h/g, getHour(date.getHours(), false));
+            case "mm":
+                return pattern.replace(/mm/g, date.getMinutes() >= 10 ? date.getMinutes() : "0" + date.getMinutes());
+            case "m":
+                return pattern.replace(/m/g, date.getMinutes());
+            case "ss":
+                return pattern.replace(/ss/g, date.getSeconds() >= 10 ? date.getSeconds() : "0" + date.getSeconds());
+            case "s":
+                return pattern.replace(/s/g, date.getSeconds());
+            default:
+                return pattern
+        }
+    }
 
     document.getElementById("datetime_out").value = pattern
 }
 
-function check(massife){
+function check(massife) {
     if (massife.filter(
-        function (x) {
-            return x === "="
-        }).length !== 1 && massife[massife.lastIndex] !== "=") {
+            function (x) {
+                return x === "="
+            }).length !== 1 && massife[massife.lastIndex] !== "=") {
         document.getElementById("calcPhrase_out").value = error
-        throw new Error()
     }
     for (let i = 0; i < massife.length; i++) {
         if (i % 2 === 0 && isNaN(massife[i])) {
             document.getElementById("calcPhrase_out").value = error
-            throw new Error()
         }
         if (i % 2 === 1 && !isNaN(massife[i])) {
             document.getElementById("calcPhrase_out").value = error
-            throw new Error()
         }
     }
     return true
 }
 
-function getHours(hours, zero) {
+function getHour(hours, zero) {
     if (hours >= 0 && hours <= 9) {
         return zero ? "0" + hours : hours
     }
